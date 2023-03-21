8ed8418b29adba5058d1d2076d54bae46a8bc3fa
Edwin Ariza <me@edwinariza.com>
2023-03-20 19:48:13 -0500

    ARCHITECT: Mejoramos Dflask para incluir submodules si los tiene.

---
+++ build.sh
@@ -0,0 +1,4 @@
+shc -f dflask.sh -o build/dflask
+shc -f dvue.sh -o build/dvue
+shc -f dflask.sh -o dflask
+shc -f dvue.sh -o dvue
--- dflask.sh
+++ dflask.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 #### DEVOPS -  EDWIN ARIZA
 #### FACILITANDO EL DESPLIEGUE DE APLICACIONES FLASK.
 echo "FLASK - START"
+FILE=.gitmodules
 if [[ $1 != "" ]]
     then
         cd $1/
@@ -10,8 +12,20 @@ if [[ $1 != "" ]]
                 echo "INCLUDE pull origin $BRANCH"
                 git checkout $BRANCH
                 git pull origin $BRANCH
+                if test -f "$FILE"
+                  then
+                    echo "UPDATING SUBMODULES"
+                    git submodule init
+                    git submodule update
+                  fi
             fi
     else
         git pull
+        if test -f "$FILE"
+          then
+            echo "UPDATING SUBMODULES"
+            git submodule init
+            git submodule update
+          fi
     fi
 exit
